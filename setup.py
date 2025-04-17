31cee4e3c7f1c3355f75ca6149f8bc592cece631
DeepakSakthivel-257 <deepaksakthi33@gmail.com>
2025-04-17 20:02:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Health Bot AI Project',
+    version= '0.0.0',
+    author= 'Deepak',
+    author_email= 'deepaksakthi33@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
